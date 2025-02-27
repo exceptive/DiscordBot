acf36c43d2fa9362b3d63e40cdaa4dc07a41f30f
AndreR <ARymarenko6077@stevenscollege.edu>
2025-02-27 14:05:24 -0500

    testing out events

--- main.py
+++ main.py
@@ -5,7 +5,29 @@ class Client(discord.Client):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello'):
+            await message.channel.send(f'hello {message.author}! Welcome to the server! Please checkout the https://discord.com/channels/1343680676792111196/1343681197754286143 for more rules and more')
+
+        # sends a message when someone reacts to a message (not practical might be removed)
+    async def on_reaction_add(self, reaction, user):
+        await reaction.message.channel.send('You reacted')
+
+        # sends a message when someone edits a message (not practical might be removed)
+    async def on_message_edit(self, before, after):
+        await before.channel.send('This messages content was edited')
+    
+      # sends message if a member leaves the server
+        async def on_member_remove(member):
+            print(f"{member.name} left the server.")  # Debugging line
+
+            channel = discord.utils.get(member.guild.text_channels, name="general")
+            if channel:
+                await channel.send(f"{member.name} has left the server. 😢")
+            else:
+             print("No 'goodbye' channel found.")
+
 
 
 intents = discord.Intents.default()
