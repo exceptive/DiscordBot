4dc26d0a355a1e5f46d99bdc8e571f52f8a9ac26
andre HOME <helpme@fuck.com>
2025-03-05 00:37:26 -0500

    better embed, need to bug fix why async commands are tripling/doubling up

--- main.py
+++ main.py
@@ -61,5 +61,10 @@ async def hello(interaction: discord.Interaction, printer: str):
 @client.tree.command(name="embed", description="embed demo", guild=GUILD_ID) 
 async def hello(interaction: discord.Interaction):
     embed = discord.Embed(title="8BRC check", url="https://www.rolimons.com/item/10159600649", description="views current statistics on 8brc", color=discord.Color.dark_purple())
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1343681197754286143/1343681197754286143/8BRC.png")
+    embed.add_field(name="Value", value="Field 1 Value", inline=False)
+    embed.add_field(name="RAP", value="Put random shit here", inline=False)
+    embed.set_footer(text="8-Bit Royal Crown (8BRC)")
+    embed.set_author(name=interaction.user.name, url="https://www.rolimons.com/item/10159600649", icon_url="https://tr.rbxcdn.com/180DAY-598f08aea9c33966821949b05a4135c1/420/420/Hat/Png/noFilter")
     await interaction.response.send_message(embed=embed)
 client.run('MTM0MzY3OTY5ODczOTEzODU2MA.GmympK.1riW1xn9-77RD9E_7d_choeQdyDoJOqy93_E0s')
