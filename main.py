109c7f175cda6503c053c2a0b7c2cfc4d81806d4
AndreR <ARymarenko6077@stevenscollege.edu>
2025-03-05 14:08:44 -0500

    polished automated commands and logs, fixed welcome and goodbye intents

--- main.py
+++ main.py
@@ -19,9 +19,15 @@ class Client(commands.Bot):
     async def on_member_join(self, member):
         channel = discord.utils.get(member.guild.text_channels, name="welcome")
         if channel:
-            await channel.send(f"Welcome {member} to the server! Please checkout the https://discord.com/channels/1343680676792111196/1343681197754286143 for more rules and more")
+            await channel.send(f"Welcome {member.mention} to the server! Please checkout the https://discord.com/channels/1343680676792111196/1346917332559073384 for more rules and more")
         else:
             print("No 'welcome' channel found.")
+
+    async def on_member_remove(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name="goodbye")
+        if channel:
+            await channel.send(f"Goodbye {member.mention}! We will miss you!")
+
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -31,9 +37,11 @@ class Client(commands.Bot):
     async def on_reaction_add(self, reaction, user):
         await reaction.message.channel.send('You reacted')
 
-        # sends a message when someone edits a message (not practical might be removed)
+        # sends a message when someone edits a message (not practical might be removed) (redirecting to logs channel)
     async def on_message_edit(self, before, after):
-        await before.channel.send('This messages content was edited')
+        log_channel = discord.utils.get(before.guild.text_channels, name="logs")
+        if log_channel:
+         await log_channel.send(f'a message has been edited by {before.author} was "{before.content}" and now is "{after.content}"')
     
         # message logger in logs
     async def on_message_delete(self, message):
